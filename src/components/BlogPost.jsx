bca555935a94ebc2cc32e659222bc1c75f550b9b
Héctor Airy <hello@hectorairy.com>
2022-10-23 18:30:49 -0500

    feat: access to browsing history using useNavigate

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,14 +1,21 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { blogdata } from "../data/blogdata";
 
 export const BlogPost = () => {
+  const navigate = useNavigate();
   const { slug } = useParams();
   const { title, content, author } = blogdata.find(
     (post) => post.slug === slug
   );
+
+  const returnToBlog = () => {
+    navigate("/blog");
+  };
+
   return (
     <>
       <h2>{title}</h2>
+      <button onClick={returnToBlog}>Volver al blog</button>
       <p>{content}</p>
       <p>{author}</p>
     </>
