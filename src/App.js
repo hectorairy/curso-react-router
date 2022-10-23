3a013f30a71c0527de2b02ba3585a8b356ccebb4
Héctor Airy <hello@hectorairy.com>
2022-10-23 17:03:56 -0500

    feat: create routes for navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { BlogPage } from "./components/BlogPage";
+import { HomePage } from "./components/HomePage";
+import { Menu } from "./components/Menu";
+import { ProfilePage } from "./components/ProfilePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="*" element={<p>Not found</p>} />
+      </Routes>
+    </HashRouter>
   );
 }
 
